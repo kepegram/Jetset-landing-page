a40fd060cf126fef64e32f929622a097ead71e36
Kadin Pegram <kpegra1@students.towson.edu>
2024-10-24 12:00:28 -0500

    Fixes

--- App.tsx
+++ App.tsx
@@ -6,8 +6,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { FIREBASE_AUTH } from "./firebase.config";
 import { useColorScheme } from "react-native";
-import { Ionicons } from "@expo/vector-icons"; // Import Ionicons for the back button
-import * as SplashScreen from "expo-splash-screen"; // Import SplashScreen from Expo
+import { Ionicons } from "@expo/vector-icons";
+import * as SplashScreen from "expo-splash-screen";
 import Welcome from "./src/screens/onboarding/welcome/welcome";
 import UserAuth from "./src/screens/onboarding/userAuth/userAuth";
 import SignUp from "./src/screens/onboarding/userAuth/signup";
@@ -32,29 +32,26 @@ SplashScreen.preventAutoHideAsync();
 
 const App: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
-  const [appIsReady, setAppIsReady] = useState(false); // State to track app readiness
+  const [appIsReady, setAppIsReady] = useState(false);
   const colorScheme = useColorScheme();
 
   useEffect(() => {
-    // Simulate a resource loading or initialization phase (like fetching user data)
     async function prepare() {
       try {
-        // You can load resources here (e.g., fonts, data)
-        await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate loading
+        await new Promise((resolve) => setTimeout(resolve, 1000));
       } catch (e) {
         console.warn(e);
       } finally {
-        setAppIsReady(true); // App is ready
+        setAppIsReady(true);
       }
     }
 
     prepare();
   }, []);
 
-  // Ensure the splash screen hides only when the app is ready
   const onLayoutRootView = useCallback(async () => {
     if (appIsReady) {
-      await SplashScreen.hideAsync(); // Hide splash screen once app is ready
+      await SplashScreen.hideAsync();
     }
   }, [appIsReady]);
 
@@ -82,11 +79,10 @@ const App: React.FC = () => {
         />
       </Pressable>
     ),
-    headerTitle: "", // No title
+    headerTitle: "",
   });
 
   if (!appIsReady) {
-    // You can return null or a loading indicator here until the app is ready
     return null;
   }
 
