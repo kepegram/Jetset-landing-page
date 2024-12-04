e605c7c53ce7952ec385edc286100ef96536290b
Kadin Pegram <kepegram1@gmail.com>
2024-12-04 11:50:43 -0600

    Finished carousel revamp

--- src/screens/onboarding/carousel/carousel.tsx
+++ src/screens/onboarding/carousel/carousel.tsx
@@ -11,6 +11,7 @@ import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../../../App";
 import { useTheme } from "../../../context/themeContext";
+import { Ionicons } from '@expo/vector-icons';
 
 type CarouselScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -21,16 +22,24 @@ const Carousel: React.FC = () => {
   const navigation = useNavigation<CarouselScreenNavigationProp>();
   const { currentTheme } = useTheme();
 
+  let swiperRef: Swiper | null = null;
+
   return (
     <Swiper
+      ref={(ref) => (swiperRef = ref)}
       style={styles.wrapper}
       showsButtons={false}
       loop={false}
       activeDotColor={currentTheme.alternate}
       dotColor={currentTheme.inactive}
+      dotStyle={styles.dotStyle}
+      activeDotStyle={styles.activeDotStyle}
+      paginationStyle={styles.paginationStyle} // Added this line
     >
+      {/* Slide 1 */}
+
       <ImageBackground
-        source={require("../../../assets/onboarding-imgs/pexels-vince-2265876.jpg")}
+        source={require("../../../assets/onboarding-imgs/city.jpeg")}
         style={styles.backgroundImage}
       >
         <View style={styles.slide1}>
@@ -38,70 +47,85 @@ const Carousel: React.FC = () => {
             <Text style={[styles.title1, { color: "white" }]}>
               Create your dream travel list
             </Text>
-            <Text
-              style={[styles.paragraph1, { color: currentTheme.textSecondary }]}
-            >
+            <Text style={[styles.paragraph1, { color: "#d1d1d1" }]}>
               Curate your own personalized travel bucket list
             </Text>
           </View>
+          <Pressable
+            style={styles.arrowButton}
+            onPress={() => swiperRef?.scrollBy(1)}
+          >
+            <Ionicons name="arrow-forward" size={30} color="white" />
+          </Pressable>
         </View>
       </ImageBackground>
 
+      {/* Slide 2 */}
+
       <ImageBackground
-        source={require("../../../assets/onboarding-imgs/hiker.jpg")}
+        source={require("../../../assets/onboarding-imgs/igloo.jpg")}
         style={styles.backgroundImage}
       >
         <View style={styles.slide2}>
+          <View style={styles.shadow} />
           <View style={styles.slide2textContainer}>
             <Text style={[styles.title2, { color: "white" }]}>
               Plan trips and explore destinations
             </Text>
-            <Text
-              style={[styles.paragraph2, { color: currentTheme.textSecondary }]}
-            >
+            <Text style={[styles.paragraph2, { color: "#d1d1d1" }]}>
               Simple tools and a powerful interface to help you plan your dream
               trip.
             </Text>
           </View>
+          <Pressable
+            style={styles.arrowButton}
+            onPress={() => swiperRef?.scrollBy(1)}
+          >
+            <Ionicons name="arrow-forward" size={30} color="white" />
+          </Pressable>
         </View>
       </ImageBackground>
 
+      {/* Slide 3 */}
+
       <ImageBackground
-        source={require("../../../assets/onboarding-imgs/city.jpeg")}
+        source={require("../../../assets/onboarding-imgs/beautiful.jpeg")}
         style={styles.backgroundImage}
       >
         <View style={styles.slide3}>
-          <View style={styles.title3Container}>
+          <View style={styles.slide3textContainer}>
             <Text style={[styles.title3, { color: "white" }]}>
-              Become{" "}
-              <Text style={{ fontSize: 50, color: currentTheme.alternate }}>
+              Become{"\n"}
+              <Text style={{ fontSize: 60, color: currentTheme.alternate }}>
                 Jetset
-              </Text>{" "}
+              </Text>{"\n"}
               Today
             </Text>
-          </View>
-          <Text
-            style={[styles.paragraph3, { color: currentTheme.textSecondary }]}
-          >
-            Join our community to start planning your travel list and explore
-            the world
-          </Text>
-          <View style={styles.buttonContainer}>
-            <Pressable
-              style={[
-                styles.authButton,
-                { backgroundColor: currentTheme.buttonBackground },
-              ]}
-              onPress={() => {
-                navigation.navigate("SignUp");
-              }}
-            >
-              <Text
-                style={[styles.buttonText, { color: currentTheme.buttonText }]}
+
+            <Text style={[styles.paragraph3, { color: "#d1d1d1" }]}>
+              Join our community to start planning your travel list and explore
+              the world
+            </Text>
+            <View style={styles.buttonContainer}>
+              <Pressable
+                style={[
+                  styles.authButton,
+                  { backgroundColor: currentTheme.buttonBackground },
+                ]}
+                onPress={() => {
+                  navigation.navigate("SignUp");
+                }}
               >
-                Get Started
-              </Text>
-            </Pressable>
+                <Text
+                  style={[
+                    styles.buttonText,
+                    { color: currentTheme.buttonText },
+                  ]}
+                >
+                  Get Started
+                </Text>
+              </Pressable>
+            </View>
           </View>
         </View>
       </ImageBackground>
@@ -118,66 +142,98 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     justifyContent: "center",
   },
+  dotStyle: {
+    width: 16,
+    height: 8,
+    borderRadius: 4,
+  },
+  activeDotStyle: {
+    width: 24,
+    height: 8,
+    borderRadius: 4,
+  },
+  shadow: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 260,
+    backgroundColor: "rgba(0, 0, 0, 0.3)",
+  },
+  paginationStyle: {
+    justifyContent: 'flex-start',
+    marginLeft: 30,
+    marginBottom: 10,
+  },
+  arrowButton: {
+    position: "absolute",
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+    padding: 10,
+    borderRadius: 25,
+    bottom: 20,
+    right: 30,
+  },
 
   // Slide 1
   slide1: {
     padding: 20,
+    flex: 1,
   },
   slide1textContainer: {
-    top: "150%",
+    top: "75%",
   },
   title1: {
-    fontSize: 28,
-    fontWeight: "bold",
+    fontSize: 38,
     marginBottom: 25,
+    fontWeight: "bold",
+    textAlign: "left",
   },
   paragraph1: {
     fontSize: 16,
-    paddingHorizontal: 20,
+    textAlign: "left",
   },
 
   // Slide 2
   slide2: {
     padding: 20,
+    flex: 1,
   },
   slide2textContainer: {
-    top: "150%",
+    top: "75%",
   },
   title2: {
-    fontSize: 28,
+    fontSize: 38,
+    marginBottom: 15,
     fontWeight: "bold",
-    marginBottom: 25,
+    textAlign: "left",
   },
   paragraph2: {
     fontSize: 16,
-    paddingHorizontal: 20,
+    textAlign: "left",
   },
 
   // Slide 3
   slide3: {
     padding: 20,
-    justifyContent: "flex-end",
     flex: 1,
   },
   slide3textContainer: {
-    top: "150%",
-  },
-  title3Container: {
-    marginBottom: 20,
+    top: "65%",
   },
   title3: {
     fontSize: 28,
+    marginBottom: 10,
     fontWeight: "bold",
+    textAlign: "left",
   },
   paragraph3: {
     fontSize: 18,
-    marginBottom: 50,
-    paddingHorizontal: 30,
+    marginBottom: 20,
+    textAlign: "left",
   },
   buttonContainer: {
     width: "100%",
     alignItems: "center",
-    marginBottom: 30,
   },
   authButton: {
     width: "70%",
@@ -188,10 +244,5 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 18,
     fontWeight: "bold",
-  },
-  arrowButton: {
-    position: "absolute",
-    bottom: 50,
-    right: 30,
   },
 });
